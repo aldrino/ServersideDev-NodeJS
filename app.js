009134003f1a6b1d628b24b3fc32bedd2efc69d8
Aldrin Oliveira <aldrin.o.247@gmail.com>
2018-07-20 07:49:22 +0530

    Passport

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 connect.then((db) => {
   console.log("Connected correctly to server");
@@ -40,25 +42,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.session);
-  if (!req.session.user) {
+function auth (req, res, next) {
+  console.log(req.user);
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
+    res.setHeader('WWW-Authenticate', 'Basic');                          
+    err.status = 401;
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+        next();
   }
 }
 
