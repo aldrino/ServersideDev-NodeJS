035d61d619f759de254604065f70d97f8bddeb43
Aldrin Oliveira <aldrin.o.247@gmail.com>
2018-07-31 19:43:46 +0530

    File Upload

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const uploadRouter = require('./routes/uploadRouter');
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 var config = require('./config');
@@ -63,6 +64,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
